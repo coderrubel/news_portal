816fe0208df445927582b91f23e5df2854fe3076
rubel-sebpo <rubel.ali@sebpo.com>
2022-09-09 17:45:13 +0600

    admin User name

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -136,14 +136,14 @@
                   <li class="dropdown user-menu">
                     <button href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
                       <img src="{{ asset('backend/assets/img/user/user.png')}}" class="user-image" alt="User Image" />
-                      <span class="d-none d-lg-inline-block">Abdus Salam</span>
+                      <span class="d-none d-lg-inline-block">{{Auth::user()->name}}</span>
                     </button>
                     <ul class="dropdown-menu dropdown-menu-right">
                       <!-- User image -->
                       <li class="dropdown-header">
                         <img src="{{ asset('backend/assets/img/user/user.png')}}" class="img-circle" alt="User Image" />
                         <div class="d-inline-block">
-                          Abdus Salam <small class="pt-1">abdus@gmail.com</small>
+                        {{Auth::user()->name}} <small class="pt-1"> {{ Auth::user()->email }}</small>
                         </div>
                       </li>
 
