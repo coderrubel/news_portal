2548f390427f6343cb7265f9e92c1b6da6abe866
coderrubel <95075500+rubel-sebpo@users.noreply.github.com>
2023-03-20 21:08:06 +0600

    last update

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,11 +1,7 @@
+@extends('admin.admin_master')
+@section('admin')
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
 
-    <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             @if (Laravel\Fortify\Features::canUpdateProfileInformation())
                 @livewire('profile.update-profile-information-form')
@@ -41,5 +37,5 @@
                 </div>
             @endif
         </div>
-    </div>
 </x-app-layout>
+@endsection
