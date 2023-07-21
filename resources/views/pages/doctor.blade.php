b34513451bca35643010b3d64f5a6c604138ab3e
coderrubel <95075500+rubel-sebpo@users.noreply.github.com>
2023-07-22 04:07:46 +0600

    doctor page slider add

--- resources/views/pages/doctor.blade.php
+++ resources/views/pages/doctor.blade.php
@@ -3,6 +3,37 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
+                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    @php
+                    $faq = DB::table('sliders')->get();
+                    @endphp
+                    @foreach($faq as $faq_list)
+                    <div class="carousel-item @if($loop->first) active @endif">
+                        <img src="{{$faq_list->image}}" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5> {{ $faq_list->title }}</h5>
+                            <p>{{$faq_list->description}}</p>
+                        </div>
+                    </div>     
+                    @endforeach  
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+                </div>
+            </div>
+            <div class="col-md-12">
                 <h2>SPECIALIST DOCTORS LIST</h2>
                 <nav class="breadcrumb-container">
                     <ol class="breadcrumb">
